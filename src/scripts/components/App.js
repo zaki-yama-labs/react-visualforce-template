4381160e320cf792a7692822064d6ad19f8e0474
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2017-02-13 23:40:30 +0900

    change App component

--- src/scripts/components/App.js
+++ src/scripts/components/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import { Button, DateInput, util } from 'react-lightning-design-system';
+import { Button, Form, Input, DateInput, util } from 'react-lightning-design-system';
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
-        <Button type="brand">foo</Button>
-        <DateInput label='DateInput #1' defaultValue='2015-12-24' defaultOpened required />
+        <h2 className='slds-m-vertical--medium'>Form Inline</h2>
+        <Form type='inline'>
+          <Input label='Text Field #1' type='text' placeholder='Input text here' />
+          <DateInput label='DateInput #1' defaultValue='2015-12-24' required />
+          <Button type='brand'>Submit</Button>
+        </Form>
       </div>
     );
   }
